773d9f17213286de74fdee7901b241a3f8575303
sajaddalir <s.dalir1997@gmail.com>
2025-06-05 16:41:09 +0330

    Start new app(homePage) & import template with make settings

--- bijak/urls.py
+++ bijak/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import create_all_forms, success_page, search_page
 
 urlpatterns = [
-    path('', create_all_forms, name='form'),
+    path('issuance/', create_all_forms, name='form'),
     path('success/', success_page, name='success'),
     path('search/', search_page, name='search'),
 ]
---
+++ homePage/static/script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('پیام شما با موفقیت ارسال شد!');
+});
