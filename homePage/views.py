b8e747d56e384fcb55c8f819fc70ae5440bdf117
sajaddalir <s.dalir1997@gmail.com>
2025-10-10 14:55:23 +0330

    Make some changes to the home page

--- homePage/views.py
+++ homePage/views.py
@@ -1,4 +1,17 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = 'base.html'
+
+
+class StaffOnlyView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
+    template_name = 'bijak/issuance_form.html'
+
+    def test_func(self):
+        return self.request.user.role in ['admin', 'staff']
 
 
 def index(request):
